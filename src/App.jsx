9a2cdff1b65a03665925f5491e2a119c4107f9ff
Rajiv Basnet <rajivbasnet1@gmail.com>
2025-11-22 13:39:21 -0800

    simple 3 words

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// simple 3 words
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
